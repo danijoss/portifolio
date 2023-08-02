9bbc9c77ef438f7cfd5929435782e82ee3f291cd
Daniel <dan.henrri@gmail.com>
2023-08-02 18:37:44 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/danijoss.png" />
+    <Titulo fontSize={20}>Daniel Silva</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure suscipit
+      aperiam earum laborum, fugit ex tenetur ipsum officia voluptates odit,
+      maiores a cupiditate quibusdam totam dolorum fugiat perspiciatis delectus.
+      Dolorum.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=danijoss&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=danijoss&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
