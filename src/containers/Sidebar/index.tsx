4003632af1b7ca3d48a1821999779d4e95b71e32
Daniel <dan.henrri@gmail.com>
2023-08-03 10:50:18 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/danijoss.png" />
-    <Titulo fontSize={20}>Daniel Silva</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Daniel Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        danijoss
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
