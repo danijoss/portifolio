a8cc6b6c24460b6103dbc7891e7c89dbf0a1ab8c
Daniel <dan.henrri@gmail.com>
2023-08-08 15:13:42 -0300

    correcao na secao sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -7,10 +7,15 @@ const Sobre = () => (
     <Titulo fontSize={16}>Sobre mim:</Titulo>
     <Paragrafo tipo="principal">
       Formado em Analise de Sistemas pela Fatec em 2016 e estudando Egenharia
-      Fron-end pela EBAC em 2023, tenho 7 anos de experiencia na area de
-      consultoria de TI e estou iniciando a migração da minha carreira para dev
-      front-end. Veja abaixo alguns repositórios do meu perfil no GitHub com
-      projetos pessoais e de estudos realizados no curso da EBAC:
+      Front-end pela EBAC em 2023.
+    </Paragrafo>
+    <Paragrafo>
+      Tenho 7 anos de experiencia na area de consultoria de TI e agora estou
+      migrando minha carreira para dev front-end.
+    </Paragrafo>
+    <Paragrafo>
+      Veja abaixo alguns repositórios do meu perfil no GitHub com projetos
+      pessoais e de estudos realizados no curso da EBAC:
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats-xi-six-23.vercel.app/api?username=danijoss&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
